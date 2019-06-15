3b4f8e19e73abca72cdf4b4373bec435d308baa6
Maxime Fabre <ehtnam6@gmail.com>
2019-06-15 11:46:52 +0200

    More design tweaks

--- examples/dashboard.php
+++ examples/dashboard.php
@@ -44,13 +44,15 @@ function statistic(string $title, $value): void
         ob_start();
         $value();
         $value = ob_get_clean();
+    } else {
+        $value = ' <h2 class="p-3 m-0 font-weight-lighter">' .$value . '</h2>';
     } ?>
     <article class="card">
-        <div class="card-body text-center d-flex p-0">
-            <h3 class="card-title text-uppercase text-right bg-primary text-white p-3 font-weight-lighter m-0 " style="width: 25%">
+        <div class="card-body text-left d-flex p-0">
+            <h3 class="card-title text-uppercase text-right bg-dark text-white p-3 font-weight-lighter m-0 " style="width: 25%">
                 <?= $title; ?>
             </h3>
-            <h2 class="p-3 m-0 font-weight-lighter text-left "><?= $value; ?></h2>
+           <?= $value ?>
         </div>
     </article>
     <?php
@@ -64,10 +66,10 @@ function statistic(string $title, $value): void
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Project dashboard</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/litera/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/pulse/bootstrap.min.css">
 </head>
 <body style="padding: 2rem">
-<section class="container-full">
+<div class="container">
     <h1>Project Dashboard</h1>
     <section class="card-grid">
         <?php statistic('Current sprint', $sprint->title); ?>
@@ -75,15 +77,14 @@ function statistic(string $title, $value): void
         <?php statistic('Issues groomed', $issuesGroomed->count()); ?>
         <?php statistic('Pending proposals', function () use ($pendingProposals): void {
     ?>
-            <ul class="list-unstyled">
+            <div class="list-group list-group-flush flex-fill">
                 <?php foreach ($pendingProposals as $proposal) { ?>
-                    <li>
-                        <a href=" <?= $proposal->getUrl(); ?>" target="_blank   ">
+                    <a class="list-group-item list-group-item-action" href=" <?= $proposal->getUrl(); ?>" target="_blank   ">
                             <?= $proposal->title; ?>
-                        </a> by <strong><?= $proposal->author; ?></strong>
-                    </li>
+                        by <strong><?= $proposal->author; ?></strong>
+                    </a>
                 <?php } ?>
-            </ul>
+            </div>
             <?php
 }); ?>
     </section>
