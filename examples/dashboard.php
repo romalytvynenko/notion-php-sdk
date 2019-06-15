4d895d4862e168c83a9fad1ab1f84d6f017c7dc0
Maxime Fabre <ehtnam6@gmail.com>
2019-06-15 11:30:21 +0200

    Tweak dashboard design

--- examples/dashboard.php
+++ examples/dashboard.php
@@ -17,6 +17,7 @@ $nextSprint = $sprints
         return $block->status === 'Future';
     });
 
+/** @var \Illuminate\Support\Collection $issuesGroomed */
 $issuesGroomed = $nextSprint
     ->getChildren()
     ->first(function (Notion\Records\Blocks\BasicBlock $block) {
@@ -25,6 +26,7 @@ $issuesGroomed = $nextSprint
     ->getCollection()
     ->getRows();
 
+/** @var CollectionRowBlock[] $pendingProposals */
 $pendingProposals = $client
     ->getBlock(getenv('URL_PROPOSALS_PAGE'))
     ->getRows()
@@ -35,6 +37,24 @@ $pendingProposals = $client
 $sprint = $sprints->first(function (CollectionRowBlock $block) {
     return $block->status === 'Current';
 });
+
+function statistic(string $title, $value): void
+{
+    if (is_callable($value)) {
+        ob_start();
+        $value();
+        $value = ob_get_clean();
+    } ?>
+    <article class="card">
+        <div class="card-body text-center d-flex p-0">
+            <h3 class="card-title text-uppercase text-right bg-primary text-white p-3 font-weight-lighter m-0 " style="width: 25%">
+                <?= $title; ?>
+            </h3>
+            <h2 class="p-3 m-0 font-weight-lighter text-left "><?= $value; ?></h2>
+        </div>
+    </article>
+    <?php
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -44,53 +64,28 @@ $sprint = $sprints->first(function (CollectionRowBlock $block) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Project dashboard</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/darkly/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/litera/bootstrap.min.css">
 </head>
 <body style="padding: 2rem">
-<div class="container-full">
+<section class="container-full">
     <h1>Project Dashboard</h1>
-    <section class="card-deck">
-        <article class="card">
-            <div class="card-header">
-                Current Sprint
-            </div>
-            <div class="card-body text-center">
-                <h1><?= $sprint->title; ?></h1>
-            </div>
-        </article>
-        <article class="card">
-            <div class="card-header">
-                Sprint deadline
-            </div>
-            <div class="card-body text-center">
-                <h1><?= $sprint->ends->format('Y-m-d'); ?></h1>
-            </div>
-        </article>
-        <article class="card">
-            <div class="card-header">
-                Issues groomed for <?= $nextSprint->title; ?>
-            </div>
-            <div class="card-body text-center">
-                <h1><?= $issuesGroomed->count(); ?></h1>
-            </div>
-        </article>
-        <article class="card">
-            <div class="card-header">
-                Pending proposals
-            </div>
-            <div class="card-body">
-                <ul>
-                    <?php foreach ($pendingProposals as $proposal) { ?>
-                        <li>
-
-                            <a href=" <?= $proposal->getUrl(); ?>" target="_blank   ">
-                                <?= $proposal->title; ?>
-                            </a> by <strong><?= $proposal->author; ?></strong>
-                        </li>
-                    <?php } ?>
-                </ul>
-            </div>
-        </article>
+    <section class="card-grid">
+        <?php statistic('Current sprint', $sprint->title); ?>
+        <?php statistic('Deadline', $sprint->ends->format('Y-m-d')); ?>
+        <?php statistic('Issues groomed', $issuesGroomed->count()); ?>
+        <?php statistic('Pending proposals', function () use ($pendingProposals): void {
+    ?>
+            <ul class="list-unstyled">
+                <?php foreach ($pendingProposals as $proposal) { ?>
+                    <li>
+                        <a href=" <?= $proposal->getUrl(); ?>" target="_blank   ">
+                            <?= $proposal->title; ?>
+                        </a> by <strong><?= $proposal->author; ?></strong>
+                    </li>
+                <?php } ?>
+            </ul>
+            <?php
+}); ?>
     </section>
 </div>
 <script
